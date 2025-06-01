a4db42fa2cd43f66adf5934b136b4e61b4259636
jrmwlk <214087631+jrmwlk@users.noreply.github.com>
2025-06-01 21:31:43 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import subprocess
 from flask import Flask, jsonify
 from traitement import run_traitement
 import os
@@ -14,5 +15,9 @@ def home():
     return "✅ Eurofos API en ligne. Accédez aux données via /data"
 
 if __name__ == '__main__':
+    print("Lancement de la récupération via main.py...")
+    subprocess.run(["python3", "main.py"], check=True)
+    print("Récupération terminée. Démarrage du serveur Flask...")
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
