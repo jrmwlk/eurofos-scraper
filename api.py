5f85002edf8bf02fb6e5a03770e65d481c652c6b
jrmwlk <214087631+jrmwlk@users.noreply.github.com>
2025-06-01 21:09:16 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from traitement import run_traitement
+import os
 
 app = Flask(__name__)
 
@@ -8,5 +9,10 @@ def data():
     result = run_traitement("debug.html")
     return jsonify(result)
 
+@app.route('/')
+def home():
+    return "✅ Eurofos API en ligne. Accédez aux données via /data"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
